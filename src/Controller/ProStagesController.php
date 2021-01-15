eaec68b95f6e57f01f9a083738f24b62fc7b9875
Cédric Luu <cluu@iutbayonne.univ-pau.fr>
2021-01-15 21:35:44 +0100

    tp1

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -8,41 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStagesController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_stages-")
-     */
-    public function index()
-    {
-        return $this->render('pro_stages/index.html.twig');
-    }
+  /**
+  * @Route("/", name="pro_stages-")
+  */
+  public function index()
+  {
+    return $this->render('pro_stages/index.html.twig', [ 'controller_name' => "ProStages", ]);
+  }
 
 
-        /**
-         * @Route("/entreprises", name="pro_stages-entreprises")
-         */
-        public function afficherEntreprises()
-        {
-            return $this->render('pro_stages/affichageEntreprises.html.twig');
-        }
+  /**
+  * @Route("/entreprises", name="pro_stages-entreprises")
+  */
+  public function afficherEntreprises()
+  {
+    return $this->render('pro_stages/affichageEntreprises.html.twig');
+  }
 
 
-                /**
-                 * @Route("/formations", name="pro_stages-formations")
-                 */
-                public function afficherFormations()
-                {
-                    return $this->render('pro_stages/affichageFormations.html.twig');
-                }
+  /**
+  * @Route("/formations", name="pro_stages-formations")
+  */
+  public function afficherFormations()
+  {
+    return $this->render('pro_stages/affichageFormations.html.twig');
+  }
 
 
 
-                                /**
-                                 * @Route("/stages", name="pro_stages-stages")
-                                 */
-                                public function afficherStages()
-                                {
-                                    return $this->render('pro_stages/affichageStages.html.twig', ['idStage' =>21]);
-                                }
+  /**
+  * @Route("/stages", name="pro_stages-stages")
+  */
+  public function afficherStages()
+  {
+    return $this->render('pro_stages/affichageStages.html.twig', ['idStage' =>21]);
+  }
 
 
 }
